dbf5bc69aaa9f469be3ef08bdc01982a71fc2817
Stevan Pivnicki <stevan314@gmail.com>
2017-12-19 19:46:00 +0100

    Devel branch first commit.

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-    <footer>
+    <footer class="site-footer">
     <?php bloginfo( 'name' ) ?> - &copy; <?php echo date('Y');?>
     </footer></footer>
 </body>
--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 get_header( );
 if(have_posts()):
     while(have_posts()):the_post();?>
-         <h1><a href="<?php the_permalink()?>"> <?php the_title();?></a></h1>
+    <article class="post">
+         <h2><a href="<?php the_permalink()?>"> <?php the_title();?></a></h2>
          <p><?php the_content();?></p>
+     </article>
     <?php endwhile;
 else:
     echo "No posts";
